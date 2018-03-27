a75511e8fb863550c14b5b437a59fb9bfdbc5b1d
Alchemy <maanoatu17@gmail.com>
2018-03-27 11:34:25 +0300

    madlib

---
+++ madlib.py
@@ -0,0 +1,27 @@
+print("Input name of a city:")
+
+city = input()
+
+print("Input name of an animal:")
+
+animal = input()
+
+print("Input name of a food:")
+
+food = input()
+
+print(f'''
+Once upon a time, deep in an ancient jungle,
+there lived a {animal}.  This {animal}
+liked to eat {food}, but the jungle had
+very little {food} to offer.  One day, an
+explorer found the {animal} and discovered
+it liked {food}.  The explorer took the
+{animal} back to {city}, where it could
+eat as much {food} as it wanted.  However,
+the {animal} became homesick, so the
+explorer brought it back to the jungle,
+leaving a large supply of {food}.
+
+The End
+''')
